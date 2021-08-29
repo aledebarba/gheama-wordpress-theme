1ec25bc18ce0cac56f683fb7174eca979406b18f
Alexandre CMC Souza <ale@alemacedo.com>
2021-08-29 16:40:38 -0300

    Code Update @aledebarba ::: update jquery to 3.6.0

--- single.php
+++ single.php
@@ -2,13 +2,13 @@
 	get_header();
 	while (have_posts()) : the_post();
 
-	if(have_rows('page_header')) :
-	while(have_rows('page_header')) : the_row();
-		get_template_part('templates/widget_intro');
-	endwhile;
-	endif;
+		if(have_rows('page_header')) :
+			while(have_rows('page_header')) : the_row();
+				get_template_part('templates/widget_intro');
+			endwhile;
+		endif;
 
-	get_template_part('templates/template_content');
+		get_template_part('templates/template_content');
 
 	endwhile;
 	get_footer();
